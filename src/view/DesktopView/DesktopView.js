73a2bc52c8ca841f97db186422d6b4a5cef544eb
Saxenaaviral11 <74315206+Saxenaaviral11@users.noreply.github.com>
2024-02-17 23:20:53 +0530

    added desktop view

--- src/view/DesktopView/DesktopView.js
+++ src/view/DesktopView/DesktopView.js
@@ -1,15 +1,23 @@
-import React from 'react'
-import './DesktopView.css'
+import React, { useState, useEffect } from "react";
+import "./DesktopView.css";
 import DesktopSidebar from "../../components/sidebarDesktop/DesktopSidebar";
 import DesktopHome from "../../components/homeDesktop/DesktopHome";
+import DesktopNotes from "../../components/notesDesktop/DesktopNotes";
 
 function DesktopView() {
+  const [selected, setSelected] = useState(""); // eslint-disable-line
+  const [notes, setNotes] = useState([]); // eslint-disable-line
+
   return (
-    <div className='desktop'>
-      <DesktopSidebar />
-      <DesktopHome />
+    <div className="desktop">
+      <DesktopSidebar selected={selected} setSelected={setSelected} />
+      {selected.length > 0 ? (
+        <DesktopNotes notes={notes} setNotes={setNotes} selected={selected} setSelected={setSelected} />
+      ) : (
+        <DesktopHome />
+      )}
     </div>
-  )
+  );
 }
 
-export default DesktopView
+export default DesktopView;
