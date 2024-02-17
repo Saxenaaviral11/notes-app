d71cd3c090c6c02b2e5748e1357601283dcd39b3
Saxenaaviral11 <74315206+Saxenaaviral11@users.noreply.github.com>
2024-02-17 23:18:47 +0530

    added landing page and sidebar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import DesktopView from "./view/DesktopView/DesktopView";
+import MobileView from "./view/MobileView/MobileView";
 
 function App() {
+  const [screenSize, setScreenSize] = useState(window.innerWidth);
+
+  const checkScreenSize = () => {
+    setScreenSize(window.innerWidth);
+  };
+
+  window.addEventListener("resize", checkScreenSize);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {screenSize > 550 ? <DesktopView /> : <MobileView />}
     </div>
   );
 }
